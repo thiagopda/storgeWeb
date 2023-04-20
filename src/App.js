a03afee52a6b384fb6fdf7763a234c76172617b1
Thiago Almeida <thiagoalmeida.ti@gmail.com>
2023-04-20 10:07:27 -0300

    change_bg_tx

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        STORGECARE - Saúde domiciliar. <code></code> load.
+        ...em construção...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
