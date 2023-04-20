809c67cb8c2e4908726f01cfb15188830d784b34
Thiago Almeida <thiagoalmeida.ti@gmail.com>
2023-04-20 09:12:03 -0300

    site_title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        STORGECARE - Saúde domiciliar. <code></code> load.
         </p>
         <a
           className="App-link"
