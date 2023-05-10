223ec5b72909550a584604c962887c9db8853fbf
Thiago Almeida <thiagoalmeida.ti@gmail.com>
2023-05-10 13:29:41 -0300

    update component to element. Switch to Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Search from './components/Search';
@@ -13,12 +13,12 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/search" component={Search} />
-          <Route path="/signup" component={SignUp} />
-          <Route path="/signin" component={SignIn} />
-        </Switch>
+        <Routes>
+         <Route exact path="/" element={<Home/>}/>
+          <Route path="/search" element={Search} />
+          <Route path="/signup" element={SignUp} />
+          <Route path="/signin" element={SignIn} />
+        </Routes>
       </div>
     </Router>
   );
